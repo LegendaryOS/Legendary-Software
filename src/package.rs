eb3e5d1aa3d9cf309f9f6947a7e1015321c662e9
VoidArc Studio <chudybyczek2137@gmail.com>
2025-10-02 11:02:53 +0200

    Update package.rs

--- src/package.rs
+++ src/package.rs
@@ -1,1 +1,12 @@
+use serde::{Deserialize, Serialize};
 
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Package {
+    pub name: String,
+    pub description: String,
+    pub icon: String,
+    pub install_command: String,
+    pub remove_command: String,
+    pub update_command: String,
+    pub check_command: String,
+}
