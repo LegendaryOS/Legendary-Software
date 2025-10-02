4efbfe3d5d83ff2def15b98f887d1601507e77ee
VoidArc Studio <chudybyczek2137@gmail.com>
2025-10-02 11:02:07 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
+use eframe::{App, NativeOptions};
+use package_manager::app::PackageManagerApp;
 
+fn main() {
+    let options = NativeOptions::default();
+    eframe::run_native(
+        "Menedżer Paczek",
+        options,
+        Box::new(|cc| Box::new(PackageManagerApp::new(cc))),
+    );
+}
